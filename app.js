5e949e9f3124a82e3cf99e1dc2f480eafad2bc8b
cyrilbouharb <55791197+cyrilbouharb@users.noreply.github.com>
2024-06-14 22:43:54 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
-'use strict';
 const switcher = document.querySelector('.btn');
 
 switcher.addEventListener('click', function() {
-    const currentTheme = document.body.classList.contains('light-theme') ? 'light' : 'dark';
-    if (currentTheme === 'light') {
-        document.body.classList.remove('light-theme');
-        document.body.classList.add('dark-theme');
-        this.textContent = "Switch to Light Mode";
-    } else {
-        document.body.classList.remove('dark-theme');
-        document.body.classList.add('light-them');
-        this.textContent = "Switch to Dark Mode";
-    }
-    console.log('current class name: ' + document.body.className);
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
+
+const className = document.body.className;
+if(className == "light-theme") {
+    this.textContent = "Switch to Dark Mode";
+} else {
+    this.textContent = "Switch to Light Mode";
+}
+console.log('current class name: ' + className);
 });
